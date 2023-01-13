f76a0516bb663e528b272fd5b2eb2c57eea7cc71
fredpell1 <fredpell1@users.noreply.github.com>
2023-01-13 16:39:49 +0000

    black formatter
    Signed-off-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/main.py
+++ src/main.py
@@ -2,12 +2,15 @@ import gymnasium as gym
 import envs
 import numpy as np
 from gymnasium import spaces
+
 env = envs.SnakeEnv(size=25)
 observation, info = env.reset()
 
 
 for _ in range(100):
-    action = env.action_space.sample()  # agent policy that uses the observation and info
+    action = (
+        env.action_space.sample()
+    )  # agent policy that uses the observation and info
     observation, reward, terminated, truncated, info = env.step(action)
     print(observation)
     if terminated or truncated:
