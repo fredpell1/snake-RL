9efb3a7d2cdf8a729bbe14bbfd786103239162ec
fredpell1 <fredericpelletier7@gmail.com>
2023-01-13 11:39:09 -0500

    initial env with body added

---
+++ src/envs/__init__.py
@@ -0,0 +1,1 @@
+from envs.snake import SnakeEnv
---
+++ src/main.py
@@ -0,0 +1,16 @@
+import gymnasium as gym
+import envs
+import numpy as np
+from gymnasium import spaces
+env = envs.SnakeEnv(size=25)
+observation, info = env.reset()
+
+
+for _ in range(100):
+    action = env.action_space.sample()  # agent policy that uses the observation and info
+    observation, reward, terminated, truncated, info = env.step(action)
+    print(observation)
+    if terminated or truncated:
+        observation, info = env.reset()
+
+env.close()
