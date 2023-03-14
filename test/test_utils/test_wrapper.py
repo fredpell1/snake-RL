0dcfc25130e1ccf9c30e73f1034b653b46331b85
fredpell1 <fredericpelletier7@gmail.com>
2023-03-14 10:56:59 -0400

    tests for wrapper

---
+++ test/test_utils/test_wrapper.py
@@ -0,0 +1,21 @@
+import pytest
+from src.envs.snake import SnakeEnv
+from src.utils.wrapper import MultiFrame
+import numpy as np
+
+@pytest.fixture
+def env():
+    env = SnakeEnv(size=10)
+    env.reset()
+    return env
+
+@pytest.fixture
+def multiframe(env):
+    return MultiFrame(env, 2)
+
+
+def test_reset_first_time_same_frame(multiframe):
+    frames, _ = multiframe.reset()
+    assert np.all(
+        np.all(frames[0][key] == frames[1][key]) for key in frames[0].keys()
+    )
